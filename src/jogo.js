027f5a34229c5bb2775b99398aedcf997c55b37e
Nathália Martins <110853018+Nathixm@users.noreply.github.com>
2022-10-12 21:17:56 +0200

    Adicionando funcao verificaAcerto

---
+++ src/jogo.js
@@ -0,0 +1,8 @@
+function verificaAcerto(sorteado, escolhido){
+    if (sorteado == escolhido) {
+        return "Parabéns! Você acertou";
+    }
+    else {
+        return "Que pena, você errou";
+    }
+}
