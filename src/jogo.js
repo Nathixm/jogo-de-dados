c5767548853f947b9d4e40a6934e3e65fa460b6c
Nathália Martins <110853018+Nathixm@users.noreply.github.com>
2022-10-13 18:55:55 +0200

    Adicionando placeholder e validacao de input

--- src/jogo.js
+++ src/jogo.js
@@ -13,13 +13,42 @@ function buscaPalpite() {
 }
 
 function sortear() {
-    var sorteado = rolarDado();
-    var palpite = buscaPalpite();
-    var mensagem = verificaAcerto(sorteado, palpite);
-    exibirResultado(mensagem);
+    var palpite = buscaPalpite();  
+    if (validaPalpite(palpite)== true){  
+        var sorteado = rolarDado();
+        var mensagem = verificaAcerto(sorteado, palpite);
+        exibirResultado(mensagem);
+    }
+    else {
+        exibirResultado("Você deve digitar um número de 1 a 6");
+    }
 }
 
 function exibirResultado(mensagem) {
     var campo = document.querySelector("div");
     campo.innerHTML = mensagem;
+}
+
+function validaPalpite(palpite){
+    if (palpite == "1"){
+        return true;
+    }
+    if (palpite == "2"){
+        return true;
+    }   
+    if (palpite == "3"){
+        return true;
+    }
+    if (palpite == "4"){
+        return true;
+    }
+    if (palpite == "5"){
+        return true;
+    }   
+    if (palpite == "6"){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
